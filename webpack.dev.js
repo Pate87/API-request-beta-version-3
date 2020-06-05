bacf6517ac84b244a7323166536cc4089e54d93d
Pate87 <patrick-schubert@gmx.de>
2020-06-05 20:03:52 +0200

    adding webpack-dev-server

--- webpack.dev.js
+++ webpack.dev.js
@@ -12,6 +12,9 @@ module.exports = {
         libraryTarget: 'var',
         library: 'Client'
     },
+    devServer: {
+        contentBase: './dist'
+    },
     module: {
         rules: [
             {
